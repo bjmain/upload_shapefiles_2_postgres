a05b9456bffe28224240362129559a86b326ee9f
Brad Main <bmain@ucdavis.edu>
2019-12-05 14:02:36 -0800

    edits2

--- run_shp2pgsql.py
+++ run_shp2pgsql.py
@@ -17,8 +17,12 @@ for f in files:
 
 #Additional Notes: 
 
-#DPR GIS shapefiles: http://wiki.spatialmanager.com/index.php/Coordinate_Systems_objects_list
-### EPSG:3310 - NAD83 / California Albers [NAD83 / California (Teale) Albers / NAD83 / CA Albers / NAD_1983_California_Teale_Albers]
+#DPR GIS shapefiles use the NAD83 datum (GRS80 ellisoid) and the California (Teale) Albers projection (translation from ellipsoid to map) 
+# spatial reference system identifier (SRID) = 3310
+# This North American Datum remains constant over time for points on the North American plate.
+# http://wiki.spatialmanager.com/index.php/Coordinate_Systems_objects_list
 
-#Gateway stuff
-###EPSG:4326 - WGS 84 [GCS_WGS_1984]
+# maps already on our DB use the World Geodetic System (WGS84), which uses the wgs84 ellipsoid, which is slightly more accurate than GRS80.
+# This datum gets updated periodically as the earths plates move using stations throughout the world.
+# spatial reference system identifier (SRID) = 4326
+
