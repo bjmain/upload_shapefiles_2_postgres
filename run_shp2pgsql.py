580c56183de0571e0545c23bfb29ec5add4a80b5
Brad Main <bmain@ucdavis.edu>
2019-12-05 14:06:57 -0800

    edit3

--- run_shp2pgsql.py
+++ run_shp2pgsql.py
@@ -10,6 +10,7 @@ files = glob.glob("*shp")
 for f in files:
     county = f.strip().split("_")[1]
     table_name = "bmain.township_"+county
+    #table_name = "bmain.section_"+county
     os.system("shp2pgsql -s 3310:4326 -I %s %s | PGPASSWORD=%s psql -U %s -h %s -p 5432 %s " % (f,table_name, config.password, config.user, config.host, config.dbname))
     
 
