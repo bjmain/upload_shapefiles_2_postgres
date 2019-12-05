6ce831c9ebebd193725ca9e3fa1cb9df4eaa1fdb
Bradley J. Main <bjmain@users.noreply.github.com>
2019-12-05 14:17:53 -0800

    Update run_shp2pgsql.py

--- run_shp2pgsql.py
+++ run_shp2pgsql.py
@@ -14,16 +14,4 @@ for f in files:
     os.system("shp2pgsql -s 3310:4326 -I %s %s | PGPASSWORD=%s psql -U %s -h %s -p 5432 %s " % (f,table_name, config.password, config.user, config.host, config.dbname))
     
 
- 
-
-#Additional Notes: 
-
-#DPR GIS shapefiles use the NAD83 datum (GRS80 ellisoid) and the California (Teale) Albers projection (translation from ellipsoid to map) 
-# spatial reference system identifier (SRID) = 3310
-# This North American Datum remains constant over time for points on the North American plate.
-# http://wiki.spatialmanager.com/index.php/Coordinate_Systems_objects_list
-
-# maps already on our DB use the World Geodetic System (WGS84), which uses the wgs84 ellipsoid, which is slightly more accurate than GRS80.
-# This datum gets updated periodically as the earths plates move using stations throughout the world.
-# spatial reference system identifier (SRID) = 4326
 
